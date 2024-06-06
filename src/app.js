f7c0cf23f3ed5f64b5819d0ba73f80bd017de659
eduardonavarro <eduardonavarro.test@gmail.com>
2024-06-05 23:06:53 -0500

    Repaso métodos get, post, put y delete

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ import userRoutes from "./routes/user.routes.js"
 const PORT = 8080;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
-app.use("/", userRoutes) 
+
+app.use("/users", userRoutes)
 
 app.get("/", (req, res) => {
     res.send(`Escuchando el puerto ${PORT}`);
@@ -15,6 +18,6 @@ app.get("/clients", (req, res) => {
     res.send(`Mi primera chamba como backend ${PORT}`);
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Escuchando en el http://localhost:${PORT}`);
 })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 
-const users = [
+let users = [
     {
         id: 1,
         name: "juan1",
@@ -34,14 +34,61 @@ const users = [
 
 const router = Router();
 
-router.get("/user", (req, res) => {
+router.get("/", (req, res) => {
     res.json({
         message: 'OK',
         users
     })
 });
 
-router.get("*", (req, res)=>{
+router.post("/", (req, res) => {
+    console.log(req.body)
+    const name = req.body.name;
+    const last_name = req.body.last_name;
+    const email = req.body.email;
+
+    if (!name || !last_name || !email) {
+        return res.status(400).json({ error: "Todos los campos son obligatorios" })
+    }
+
+    let id = users[users.length - 1].id + 1;
+    users.push({ id, name, last_name, email });
+    res.status(201).json({ id })
+});
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const { name, last_name } = req.body;
+
+    const user = users.find(u => u.id === parseInt(id));
+
+    if (user) {
+        user.name = name;
+        user.last_name = last_name;
+        res.status(200).json({ mensaje: `Usuario con id ${id} actualizado`, ...user })
+    } else {
+        res.status(404).json({ error: `User no encontrado con el id ${id}` })
+    }
+
+});
+
+router.delete("/:id", (req, res) => {
+
+    const id = req.params.id;
+    const user = users.some(u => u.id === parseInt(id));
+
+    if(user){
+        users = users.filter(u => u.id !== parseInt(id));
+        res.status(200).json({ mensaje: `Usuario con id ${id} eliminado` })
+    } else {
+        res.status(404).json({ error: `User no encontrado con el id ${id}` })
+    }
+
+    
+
+})
+
+router.get("*", (req, res) => {
     res.status(400)
     res.json(`NOT FOUND`)
 })
