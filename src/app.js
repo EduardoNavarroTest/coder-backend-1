10277dfd7d676f208a7af2bf87959fc6e9fcd4fc
eduardonavarro <eduardonavarro.test@gmail.com>
2024-06-25 22:50:40 -0500

    Primeros cambios para la segunda preentrega

--- src/app.js
+++ src/app.js
@@ -1,17 +1,64 @@
 import express from "express";
+import exphbs from "express-handlebars";
+import { Server } from "socket.io";
 import cartsRouter from "./routes/carts.router.js"
 import productsRouter from "./routes/products.router.js"
+import viewsRouter from "./routes/views.router.js";
+import ProductManager from "./class/productManager.js";
 
 const PORT = 8080;
 const app = express();
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static("./src/public"));
+
+//Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
 // Rutas
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
 
-app.listen(PORT, () => {
+//Listener
+const httpServer = app.listen(PORT, () => {
     console.log(`Escuchando en el http://localhost:${PORT}`);
 })
+
+const productManager = new ProductManager("./src/db/products.json")
+
+const io = new Server(httpServer);
+
+io.on("connection", () => {
+    console.log("User conected...")
+})
+
+
+/* Adicional 
+
+app.use(express.static("./src/public"));
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "src/public/img")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("imagenclave"), (req, res) => {
+    res.send("upload");
+})
+
+*/
+
+//Voy por minuto 21:19
