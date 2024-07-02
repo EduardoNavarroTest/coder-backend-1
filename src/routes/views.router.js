ef230fda7e9dd37aa4e6cfdecc5c56eedff6490f
eduardonavarro <eduardonavarro.test@gmail.com>
2024-07-01 21:00:51 -0500

    Home del handlebars para la segunda entrega

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
+import ProductManager from "../class/productManager.js"
+
 const router = Router();
+const productManager = new ProductManager("./src/db/products.json");
 
 router.get("/realtimeproducts", async (req, res) => {
     res.render("realtimeproducts");
 });
 
-router.get("/productos", async (req, res) => {
-    res.render("home")
+router.get("/home", async (req, res) => {
+    try {
+        const products = await productManager.getProducts();
+        res.render("home", { products });
+    } catch (e) {
+        console.log(`Error => ${e}`);
+        res.status(500).send(`Error when recovering products => ${e}`);
+    }
 })
 
 
