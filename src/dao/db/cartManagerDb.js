7af2f0458504c99ef0aae4c470cdab0f640db3a2
Eduardo Enrique Navarro Echeverria <eenavarro@sprc.com.co>
2024-07-30 11:45:10 -0500

    Cambios para la entrega final

--- src/dao/db/cartManagerDb.js
+++ src/dao/db/cartManagerDb.js
@@ -1,7 +1,6 @@
 import CartModel from "../fs/data/cart.model.js";
 
 class CartManager {
-
     createCart = async () => {
         try {
             const newCart = new CartModel({ products: [] });
@@ -10,7 +9,7 @@ class CartManager {
         } catch (error) {
             console.log(`Error: ${error}`);
         }
-    }
+    };
 
     addCart = async (cartId, productId, quantity) => {
         // Controlar que todos los campos existan
@@ -21,53 +20,53 @@ class CartManager {
 
         try {
             const cart = await this.getCartById(cartId);
-            const existsProduct = cart.products.find(item => item.product._id.toString() === productId);
+            const existsProduct = cart.products.find(
+                (item) => item.product._id.toString() === productId
+            );
 
             if (existsProduct) {
                 existsProduct.quantity += quantity;
             } else {
-                cart.products.push({ product: productId, quantity })
+                cart.products.push({ product: productId, quantity });
             }
 
             cart.markModified("products");
-            await cart.save()
+            await cart.save();
             return cart;
-
         } catch (error) {
-            console.log(`Error: ${error}`)
+            console.log(`Error: ${error}`);
         }
-
-
-    }
+    };
 
     getCarts = async () => {
         const cart = await CartModel.find();
         return cart || `Carts Not Found`;
-    }
+    };
 
     getCartById = async (id) => {
         try {
             const cart = await CartModel.findById(id);
             return cart || `Cart Not Found`;
         } catch (error) {
-            console.log("Error find cart: " + error)
+            console.log("Error find cart: " + error);
         }
-    }
+    };
 
     removeProductCart = async (cid, pid) => {
         try {
             const cart = await CartModel.findById(cid);
             if (!cart) {
-                return 'Cart Not Found';
+                return "Cart Not Found";
             }
 
             // Encuentra el índice del producto en el array de productos del carrito
-            const productIndex = cart.products.findIndex(product => product.product._id.toString() === pid);
-
+            const productIndex = cart.products.findIndex(
+                (product) => product.product._id.toString() === pid
+            );
 
             // Si el producto no se encuentra, devuelve un mensaje
             if (productIndex === -1) {
-                return 'Product Not Found in Cart';
+                return "Product Not Found in Cart";
             }
 
             // Elimina el producto del array de productos
@@ -80,7 +79,62 @@ class CartManager {
         } catch (error) {
             console.log("Error find product in cart: " + error);
         }
-    }
+    };
+
+    updateQuantityProductCart = async (cid, pid, quantity) => {
+        try {
+            const cart = await this.getCartById(cid);
+            const existsProduct = cart.products.find(
+                (item) => item.product._id.toString() === pid
+            );
+
+            if (existsProduct) {
+                existsProduct.quantity = quantity;
+            } else {
+                console.log("Product does not exist in the cart");
+                return "Product does not exist in the cart";
+            }
+
+            cart.markModified("products");
+            await cart.save();
+            return cart;
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }
+    };
+
+    cleanCart = async (cid) => {
+        try {
+            const cart = await this.getCartById(cid);
+
+            if (cart) {
+                cart.products = [];
+            } else {
+                console.log("Cart not exists");
+                return "Cart not exists";
+            }
+            await cart.save();
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }
+    };
+
+    updateProductsCart = async (cid, products) => {
+        try {
+            const cart = await this.getCartById(cid);
+
+            if (cart) {
+                cart.products = products;
+            } else {
+                console.log("Cart not exists");
+                return "Cart not exists";
+            }
+            await cart.save();
+            console.log("Products update in the Cart");
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }
+    };
 }
 
 export default CartManager;
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -2,7 +2,6 @@ import express from "express";
 import CartManager from "../dao/db/cartManagerDb.js";
 const router = express.Router();
 
-
 const cartManager = new CartManager();
 
 router.get("/", async (req, res) => {
@@ -11,15 +10,13 @@ router.get("/", async (req, res) => {
         const carts = await cartManager.getCarts();
 
         limit ? res.json(carts.slice(0, limit)) : res.json(carts);
-
     } catch (e) {
         console.error("Error recovering the carts", e);
         res.status(500).json({
-            e: "Server Error"
+            e: "Server Error",
         });
     }
 });
-
 
 router.get("/:cid", async (req, res) => {
     const id = req.params.cid;
@@ -29,7 +26,7 @@ router.get("/:cid", async (req, res) => {
     } catch (e) {
         console.error("Error recovering the product", e);
         res.status(500).json({
-            e: "Server Error"
+            e: "Server Error",
         });
     }
 });
@@ -38,16 +35,15 @@ router.post("/", async (req, res) => {
     try {
         await cartManager.createCart();
         res.status(201).json({
-            message: "Cart successfully create"
+            message: "Cart successfully create",
         });
     } catch (e) {
         console.error("Error creating cart", e);
         res.status(500).json({
-            e: "Server Error"
+            e: "Server Error",
         });
     }
-
-})
+});
 
 router.post("/:cid/product/:pid", async (req, res) => {
     const cid = req.params.cid;
@@ -56,12 +52,12 @@ router.post("/:cid/product/:pid", async (req, res) => {
     try {
         await cartManager.addCart(cid, pid, quantity);
         res.status(201).json({
-            message: "Product successfully added to cart"
+            message: "Product successfully added to cart",
         });
     } catch (e) {
         console.error("Error adding product to cart", e);
         res.status(500).json({
-            e: "Server Error"
+            e: "Server Error",
         });
     }
 });
@@ -72,12 +68,61 @@ router.delete("/:cid/product/:pid", async (req, res) => {
     try {
         await cartManager.removeProductCart(cid, pid);
         res.status(201).json({
-            message: "Product remove successfully"
+            message: "Product remove successfully",
         });
     } catch (e) {
         console.error("Error remove product on cart", e);
         res.status(500).json({
-            e: "Server Error"
+            e: "Server Error",
+        });
+    }
+});
+
+router.put("/:cid", async (req, res) => {
+    const cid = req.params.cid;
+    const products = req.body.products;
+    console.log(products);
+    try {
+        await cartManager.updateProductsCart(cid, products); ////pppppppppendiebte
+        res.status(201).json({
+            message: "Products update successfully",
+        });
+    } catch (e) {
+        console.error("Error update products on cart", e);
+        res.status(500).json({
+            e: "Server Error",
+        });
+    }
+});
+
+router.put("/:cid/product/:pid", async (req, res) => {
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+    const quantity = req.body.quantity || 1;
+    try {
+        await cartManager.updateQuantityProductCart(cid, pid, quantity);
+        res.status(201).json({
+            message: "Product update successfully",
+        });
+    } catch (e) {
+        console.error("Error update quantity", e);
+        res.status(500).json({
+            e: "Server Error",
+        });
+    }
+});
+
+router.delete("/:cid", async (req, res) => {
+    const cid = req.params.cid;
+    try {
+        await cartManager.cleanCart(cid);
+        res.status(201).json({
+            message: "Cart clean successfully",
+        });
+    } catch (e) {
+        console.error("Error clean cart", e);
+        res.status(500).json({
+            e: "Server Error",
         });
     }
 });
